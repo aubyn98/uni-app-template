9fbf7699283e923b1ea7e3a09529e37a7d52ada5
aubyn <aubyn@vip.qq.com>
2021-08-28 09:53:45 +0800

    change date tool

--- common/utils/date.js
+++ common/utils/date.js
@@ -1,5 +1,6 @@
 export default {
 	getDate(time = Date.now(), fmt = 'yyyy-MM-dd') {
+		typeof time === 'string' && (time = new Date(time).getTime());
 		const date = new Date(time + 28800000).toISOString().replace('T',' ')
 		if(fmt === 'yyyy-MM-dd hh:mm') return date.slice(0,16)
 		if(fmt === 'yyyy-MM-dd hh:mm:ss') return date.slice(0,19)
