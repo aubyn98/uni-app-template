0727d92f8885f0e8ec8a2e0b00ff925009e463dd
aubyn <aubyn@vip.qq.com>
2024-01-02 11:05:46 +0800

    update

--- common/utils/wx/authorize.js
+++ common/utils/wx/authorize.js
@@ -1,14 +1,18 @@
 const scopeDict = {
-	userInfo: 'scope.userInfo', // 用户信息 wx.getUserInfo
-	userLocation: 'scope.userLocation', // 地理位置 wx.getLocation, wx.chooseLocation	
-	userLocationBackground: 'scope.userLocationBackground', // 后台定位 wx.startLocationUpdateBackground	
+	userLocation: 'scope.userLocation', // 	精确地理位置 wx.getLocation, wx.startLocationUpdate
+	userFuzzyLocation: 'scope.userFuzzyLocation', // 模糊地理位置 wx.getFuzzyLocation	
+	userLocationBackground: 'scope.userLocationBackground', // 后台定位 wx.startLocationUpdateBackground
+	record: 'scope.record', // 	麦克风 live-pusher组件, wx.startRecord, wx.joinVoIPChat, RecorderManager.start
+	camera: 'scope.camera' ,// 摄像头 camera组件, live-pusher组件, wx.createVKSession
+	bluetooth: 'scope.bluetooth' ,// 蓝牙 wx.openBluetoothAdapter, wx.createBLEPeripheralServer
+	writePhotosAlbum: 'scope.writePhotosAlbum', // 	添加到相册 wx.saveImageToPhotosAlbum, wx.saveVideoToPhotosAlbum
+	addPhoneContact: 'scope.addPhoneContact', // 	添加到联系人 wx.addPhoneContact
+	addPhoneCalendar: 'scope.addPhoneCalendar', // 	添加到日历 	wx.addPhoneRepeatCalendar, wx.addPhoneCalendar
+	werun: 'scope.werun', // 	微信运动步数 wx.getWeRunData
 	address: 'scope.address', // 通讯地址（已取消授权，可以直接调用对应接口） wx.chooseAddress	
 	invoiceTitle: 'scope.invoiceTitle', // 	发票抬头（已取消授权，可以直接调用对应接口） wx.chooseInvoiceTitle
 	invoice: 'scope.invoice', // 	获取发票（已取消授权，可以直接调用对应接口） wx.chooseInvoice
-	werun: 'scope.werun', // 	微信运动步数 wx.getWeRunData
-	record: 'scope.record', // 	录音功能 wx.startRecord
-	writePhotosAlbum: 'scope.writePhotosAlbum', // 	保存到相册 wx.saveImageToPhotosAlbum, wx.saveVideoToPhotosAlbum
-	camera: 'scope.camera' // 摄像头 camera 组件	
+	userInfo: 'scope.userInfo', // 用户信息 wx.getUserInfo 用户信息（小程序已回收，请使用头像昵称填写，小游戏可继续调用）
 }
 export function authorize({
 	withSubscriptions,
