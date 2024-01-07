0f0b86fb3385e14cdb25bf1df2f29f1144579cef
aubyn <aubyn@vip.qq.com>
2024-01-07 09:41:59 +0800

    update

---
+++ common/utils/project.js
@@ -0,0 +1,50 @@
+import apis from '@/common/apis'
+import {
+	getSearchParams
+} from './magic'
+
+
+export function showToast(title) {
+	uni.showToast({
+		icon: 'none',
+		title
+	})
+}
+
+export function scanBarcode() {
+	uni.scanCode({
+		scanType: ['qrCode', 'barCode'],
+		success: (e) => {
+			if (e.result) {
+				apis.scanGoodsCode({
+					storeId: store.state.storeId,
+					barcode: e.result
+				}).then(res => {
+					if (res.status) {
+						uni.showToast({
+							title: '扫码成功！',
+							icon: 'none',
+							duration: 1000
+						})
+						/* uni.navigateTo({
+							url: `/pages/subPackage/goodsdetail/goodsdetail?goodsId=${res.data.id}&formSearch=1`
+						}) */
+					} else {
+						uni.showToast({
+							title: '没有找到该商品',
+							icon: 'none',
+							duration: 1000
+						})
+					}
+				})
+			}
+		},
+		fail: (e) => {
+			// uni.showToast({
+			// 	title: '扫码失败',
+			// 	icon: 'none',
+			// 	duration: 1000
+			// })
+		}
+	})
+}
