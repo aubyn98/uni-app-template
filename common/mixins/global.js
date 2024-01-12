cbc3b9ba540449d4fc03c42025d6e15c7266560f
aubyn <aubyn@vip.qq.com>
2024-01-12 15:43:29 +0800

    update

--- common/mixins/global.js
+++ common/mixins/global.js
@@ -1,13 +1,38 @@
+import {
+	getRawType
+} from '@/common/utils'
 export default {
 	data() {
 		return {
-			onShowEventKey: `[${uni.$u.page()}]onShow`
+			mixLoadingCount: 0,
+			mixLoading: false
 		}
 	},
-	onShow() {
-		uni.$emit(this.onShowEventKey)
-	},
 	methods: {
+		startLoading() {
+			this.mixLoadingCount++
+			this.mixLoading = true
+		},
+		endLoading() {
+			if (this.mixLoadingCount > 0) this.mixLoadingCount--
+			if (this.mixLoadingCount === 0) this.mixLoading = false
+		},
+		callMixLoading(val, ...argvs) {
+			this.startLoading();
+			const valType = getRawType(v)
+			if (['Function', 'AsyncFunction'].includes(valType)) {
+				return val(...argvs).finally(() => {
+					this.endLoading();
+				})
+			}
+			if (valType === 'Promise') {
+				return val.finally(() => {
+					this.endLoading();
+				})
+			}
+			this.endLoading();
+			return val
+		},
 		goBack() {
 			const pages = getCurrentPages()
 			pages.length > 1 ? uni.navigateBack() : uni.switchTab({
