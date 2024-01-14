9382bb6af00dbdca8afe25f97bcfdc4d4d8e3df8
aubyn <aubyn@vip.qq.com>
2024-01-14 15:41:50 +0800

    update

--- common/config/index.js
+++ common/config/index.js
@@ -6,8 +6,8 @@ export const Pro = {
 	baseURL: 'https://mp.api.360hwj.com',
 }
 
-export const ENUMS = {
+export const ENUMS = Object.freeze({
 	TOKEN_KEY: 'x-fronted-token',
-}
+})
 
 export default process.env.NODE_ENV === 'development' ? Dev : Pro
