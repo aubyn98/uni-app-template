0ca0fb7b61efa844e4a5f54b47f374d0aa98f796
aubyn <aubyn@vip.qq.com>
2024-01-15 10:12:57 +0800

    update

--- common/utils/validate.js
+++ common/utils/validate.js
@@ -21,33 +21,6 @@ export function validURL(url) {
 }
 
 /**
- * @param {string} str
- * @returns {Boolean}
- */
-export function validLowerCase(str) {
-  const reg = /^[a-z]+$/;
-  return reg.test(str);
-}
-
-/**
- * @param {string} str
- * @returns {Boolean}
- */
-export function validUpperCase(str) {
-  const reg = /^[A-Z]+$/;
-  return reg.test(str);
-}
-
-/**
- * @param {string} str
- * @returns {Boolean}
- */
-export function validAlphabets(str) {
-  const reg = /^[A-Za-z]+$/;
-  return reg.test(str);
-}
-
-/**
  * @param {string} email
  * @returns {Boolean}
  */
