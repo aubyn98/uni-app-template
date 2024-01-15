fb29434fb65253b7d7d6c66b9a18b9fd1b079466
aubyn <aubyn@vip.qq.com>
2024-01-15 10:20:40 +0800

    update

--- common/utils/validate.js
+++ common/utils/validate.js
@@ -7,7 +7,7 @@
  * @returns {Boolean}
  */
 export function isExternal(path) {
-  return /^(https?:|mailto:|tel:)/.test(path);
+	return /^(https?:|mailto:|tel:)/.test(path);
 }
 
 /**
@@ -15,9 +15,9 @@ export function isExternal(path) {
  * @returns {Boolean}
  */
 export function validURL(url) {
-  const reg =
-    /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
-  return reg.test(url);
+	const reg =
+		/^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+	return reg.test(url);
 }
 
 /**
@@ -25,8 +25,9 @@ export function validURL(url) {
  * @returns {Boolean}
  */
 export function validEmail(email) {
-  const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return reg.test(email);
+	const reg =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return reg.test(email);
 }
 
 /**
@@ -34,8 +35,8 @@ export function validEmail(email) {
  * @returns {Boolean}
  */
 export function validDate(str) {
-  const reg = /^\d{4}-\d{1,2}-\d{1,2}/;
-  return reg.test(str);
+	const reg = /^\d{4}-\d{1,2}-\d{1,2}/;
+	return reg.test(str);
 }
 
 /**
@@ -43,8 +44,8 @@ export function validDate(str) {
  * @returns {Boolean}
  */
 export function validPhone(str) {
-  const reg = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
-  return reg.test(str);
+	const reg = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
+	return reg.test(str);
 }
 
 /**
@@ -52,8 +53,9 @@ export function validPhone(str) {
  * @returns {Boolean}
  */
 export function validIDCard(str) {
-  const reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
-  return reg.test(str);
+	const reg =
+		/^([1-6][1-9]|50)\d{4}(18|19|20)\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$|^([1-6][1-9]|50)\d{4}\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\d{3}$/;
+	return reg.test(str);
 }
 
 /**
@@ -61,7 +63,6 @@ export function validIDCard(str) {
  * @returns {Boolean}
  */
 export function validPostalCode(str) {
-  const reg = /[1-9]\d{5}(?!\d)/;
-  return reg.test(str);
+	const reg = /[1-9]\d{5}(?!\d)/;
+	return reg.test(str);
 }
-
