0b6d54ff20a2da5ae07c05c545eecb61b9f1c913
aubyn <aubyn@vip.qq.com>
2024-01-19 14:50:28 +0800

    update

--- common/config/index.js
+++ common/config/index.js
@@ -10,27 +10,10 @@ export const BASE_URL = API_ADDRESS[IS_DEV ? 'DEV' : 'PRO']
 /** token的键名 */
 export const TOKEN_KEY = 'x-fronted-token'
 
-/** 默认商品图片 */
-export const DEFAULT_GOODS_IMG = 'http://image.360hwj.com/store/empty.png'
-
-/** 配送类型 */
-export const DELIVERY_TYPES = Object.freeze({
-	/** 门店配送 */
-	SEND: 'store_send',
-	/** 门店自提 */
-	TAKE: 'member_take'
-})
-
 /** 支付方式 */
 export const PAY_METHODS = Object.freeze({
 	/** 微信支付(小程序) */
 	WX_MINI: 'wexpayMiniAppPaymentPlugin',
 	/** 货到付款  */
 	CASH: 'cod',
-})
-
-/** tabBar页面跳转键名 */
-export const PAGE_JUMP_KEYS = Object.freeze({
-	/** 跳转找药页面参数 存储缓存的键 */
-	FIND_MEDICINE: 'jump-findMedicine',
 })
