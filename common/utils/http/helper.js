77d89ade5511b6c2c1ae19a0e3a468c574c08c14
aubyn <aubyn@vip.qq.com>
2024-08-14 09:09:47 +0800

    change utils

--- common/utils/http/helper.js
+++ common/utils/http/helper.js
@@ -1,3 +1,6 @@
+import {
+	cloneDeepWithDescriptors,
+} from '../object'
 export const CONTENT_TYPES = Object.freeze({
 	/** json */
 	JSON: 'application/json;charset=UTF-8',
@@ -78,5 +81,205 @@ export function endLoading() {
 	if (loadingCount > 0) loadingCount--
 	if (loadingCount === 0) {
 		uni.hideLoading()
+	}
+}
+
+function simplify(_request, type) {
+	return function(url, params, config, options) {
+		return _request(url, type, params, config, options)
+	}
+}
+
+export function createRequest(defaultConfig, defaultOpts) {
+
+	function _request(url, method, params, config, options) {
+		const source = CancelToken.source()
+
+		const argumentsCache = cloneDeepWithDescriptors(arguments)
+		options = {
+			loading: true,
+			qs: true,
+			showError: true,
+			resInterceptor: (res) => res.data,
+			errInterceptor: () => void 0,
+			...defaultOpts,
+			...options
+		}
+
+		config = {
+			...defaultConfig,
+			...config
+		}
+
+		let {
+			headers,
+			...configRes
+		} = config
+
+		if (typeof headers !== 'function') headers = () => headers
+
+		options.loading && startLoading()
+		let closeLoading = () => {
+			options.loading && endLoading()
+			closeLoading = null
+		}
+
+		return promisifyRequest('request', {
+			cancelToken: source.token,
+			method,
+			url: url.startsWith('http') ? url : config.baseURL ? config.baseURL + url : url,
+			data: params,
+			header: {
+				'Content-Type': method.toUpperCase() === 'GET' ? CONTENT_TYPES.JSON : options.qs ?
+					CONTENT_TYPES.FORM_URLENCODED : CONTENT_TYPES.JSON,
+				...headers()
+			},
+			...configRes
+		}).then(res => {
+			closeLoading()
+			return options.resInterceptor(res, options, () => _request(...argumentsCache))
+		}).catch(e => {
+			options.errInterceptor(e)
+			closeLoading && closeLoading()
+			return Promise.reject(e)
+		})
+	}
+	_request.get = simplify(_request, 'get')
+	_request.post = simplify(_request, 'post')
+	return _request
+}
+
+export function createUploadFile(defaultConfig, defaultOpts) {
+	return function _uploadFile(url, params, config, options) {
+		const source = CancelToken.source()
+
+		const argumentsCache = cloneDeepWithDescriptors(arguments)
+		params = {
+			keyName: 'file',
+			filePath: '',
+			...params
+		}
+
+		options = {
+			loading: true,
+			showError: true,
+			resInterceptor: (res) => res.data,
+			errInterceptor: () => void 0,
+			...defaultOpts,
+			...options
+		}
+
+		config = {
+			...defaultConfig,
+			...config
+		}
+
+		let {
+			headers,
+			...configRes
+		} = config
+
+		if (typeof headers !== 'function') headers = () => headers
+
+		const {
+			keyName,
+			filePath,
+			...formData
+		} = params
+
+		if (!filePath.startsWith('http://tmp') && !filePath.startsWith('wxfile://tmp')) return Promise.resolve({
+			data: filePath
+		})
+
+		options.loading && startLoading('上传中...')
+		let closeLoading = () => {
+			options.loading && endLoading()
+			closeLoading = null
+		}
+
+		return promisifyRequest('uploadFile', {
+				cancelToken: source.token,
+				url: url.startsWith('http') ? url : config.baseURL ? config.baseURL + url : url,
+				filePath,
+				name: keyName,
+				formData,
+				header: {
+					...headers()
+				},
+				...configRes
+			})
+			.then(res => {
+				closeLoading()
+				try {
+					res.data = JSON.parse(res.data)
+					return options.resInterceptor(res, options, () => _uploadFile(...argumentsCache))
+				} catch (e) {
+					return Promise.reject({
+						type: 'code error',
+						res: e
+					})
+				}
+			})
+			.catch(e => {
+				options.errInterceptor(e)
+				closeLoading && closeLoading()
+				return Promise.reject(e)
+			})
+
+	}
+}
+
+export function createDownloadFile(defaultConfig, defaultOpts) {
+	return function _downloadFile(url, params, config, options) {
+		const source = CancelToken.source()
+
+		options = {
+			loading: true,
+			errInterceptor: () => void 0,
+			...defaultOpts,
+			...options
+		}
+
+		config = {
+			...defaultConfig,
+			...config
+		}
+
+		let {
+			headers,
+			...configRes
+		} = config
+
+		if (typeof headers !== 'function') headers = () => headers
+
+		const hasParams = typeof params === 'object' && params !== null
+		if (hasParams) {
+			params = Object.keys(params).reduce((prev, k) => {
+				const val = params[k]
+				val && prev.push(encodeURIComponent(k) + '=' + encodeURIComponent(val))
+				return prev
+			}, []).join('&')
+		}
+		options.loading && startLoading('下载中...')
+		let closeLoading = () => {
+			options.loading && endLoading()
+			closeLoading = null
+		}
+		return promisifyRequest('downloadFile', {
+			cancelToken: source.token,
+			url: (url.startsWith('http') ? url : config.baseURL ? config.baseURL + url : url) + (hasParams ?
+				'?' + params : ''),
+			header: {
+				...headers()
+			},
+			...configRes
+		}).then((res) => {
+			closeLoading()
+			return res
+		}).catch((e) => {
+			options.errInterceptor(e)
+			closeLoading && closeLoading()
+			return Promise.reject(e)
+		})
 	}
 }
--- common/utils/http/index.js
+++ common/utils/http/index.js
@@ -5,23 +5,30 @@ import {
 import store from '@/store'
 import {
 	hasOwnProperty,
-	cloneDeepWithDescriptors,
 } from '../object'
 import {
 	showToast
 } from '../project'
 import {
-	CONTENT_TYPES,
-	CancelToken,
-	promisifyRequest,
-	startLoading,
-	endLoading
+	createRequest,
+	createUploadFile,
+	createDownloadFile
 } from './helper'
-import {
-	getSearchParams
-} from '../magic'
 
-function checkStatus(res, options, reloadFn) {
+function errInterceptor(e) {
+	const pages = uni.$u.page()
+	if ((e.type == 'fail' || e.res.statusCode == 502) && pages != '/pages/errorPage/errorPage') {
+		uni.$u.route({
+			url: '/pages/errorPage/errorPage',
+			type: 'redirectTo',
+			params: {
+				type: e.res.statusCode == 502 ? '502' : 'fail'
+			}
+		})
+	}
+}
+
+function resInterceptor(res, options, reloadFn) {
 	const data = res.data
 	if (hasOwnProperty(data, 'status') && !data.status) {
 		if (['invalidAuthorization' /* , 'parameterMustBeNotnull' */ ].includes(data.responseCode)) {
@@ -36,183 +43,58 @@ function checkStatus(res, options, reloadFn) {
 	return data
 }
 
-// 106
-export function downloadFile(url, params, config = {}, options = {}) {
-	const source = CancelToken.source()
 
-	options = {
-		loading: true,
-		...options
-	}
 
-	const {
-		headers,
-		...configRes
-	} = config
-
-	const token = store.state.user.token
-
-	const hasParams = typeof params === 'object' && params !== null
-	if (hasParams) {
-		params = Object.keys(params).reduce((prev, k) => {
-			const val = params[k]
-			val && prev.push(encodeURIComponent(k) + '=' + encodeURIComponent(val))
-			return prev
-		}, []).join('&')
-	}
-	options.loading && startLoading('下载中...')
-	let closeLoading = () => {
-		options.loading && endLoading()
-		closeLoading = null
-	}
-	return promisifyRequest('downloadFile', {
-		cancelToken: source.token,
-		url: url.startsWith('http') ? url : BASE_URL + url + (hasParams ? '?' + params : ''),
-		header: {
+export const downloadFile = createDownloadFile({
+	baseURL: BASE_URL,
+	headers() {
+		const token = store.state.user.token
+		return {
 			...(token && {
 				[TOKEN_KEY]: token
 			}),
-			...headers
-		},
-		...configRes
-	}).then((res) => {
-		closeLoading()
-		return res
-	}).catch((e) => {
-		closeLoading && closeLoading()
-		return Promise.reject(e)
-	})
-}
-
-export function uploadFile(url, params, config = {}, options = {}) {
-	const source = CancelToken.source()
-
-	const argumentsCache = cloneDeepWithDescriptors(arguments)
-	params = {
-		keyName: 'file',
-		filePath: '',
-		...params
+		}
 	}
+}, {
+	errInterceptor
+})
 
-	options = {
-		loading: true,
-		showError: true,
-		...options
+
+
+export const uploadFile = createUploadFile({
+	baseURL: BASE_URL,
+	headers() {
+		const token = store.state.user.token
+		return {
+			...(token && {
+				[TOKEN_KEY]: token
+			}),
+		}
 	}
-
-	const {
-		headers,
-		...configRes
-	} = config
-
-	const token = store.state.user.token
-
-	const {
-		keyName,
-		filePath,
-		...formData
-	} = params
-
-	if (!filePath.startsWith('http://tmp') && !filePath.startsWith('wxfile://tmp')) return Promise.resolve({
-		data: filePath
-	})
-
-	options.loading && startLoading('上传中...')
-	let closeLoading = () => {
-		options.loading && endLoading()
-		closeLoading = null
-	}
-
-	return promisifyRequest('uploadFile', {
-			cancelToken: source.token,
-			url: url.startsWith('http') ? url : BASE_URL + url, //仅为示例，非真实的接口地址
-			filePath,
-			name: keyName,
-			formData,
-			header: {
-				...(token && {
-					[TOKEN_KEY]: token
-				}),
-				...headers
-			},
-			...configRes
-		})
-		.then(res => {
-			closeLoading()
-			try {
-				res.data = JSON.parse(res.data)
-				return checkStatus(res, options, () => uploadFile(...argumentsCache))
-			} catch (e) {
-				return Promise.reject({
-					type: 'code error',
-					res: e
-				})
-			}
-		})
-		.catch(e => {
-			closeLoading && closeLoading()
-			return Promise.reject(e)
-		})
-
-}
-
-function simplify(type) {
-	return function(url, params, config, options) {
-		return request(url, type, params, config, options)
-	}
-}
-request.get = simplify('get')
-request.post = simplify('post')
+}, {
+	resInterceptor,
+	errInterceptor
+})
 
 
 
-export function request(url, method, params, config = {}, options = {}) {
-	const source = CancelToken.source()
 
-	const argumentsCache = cloneDeepWithDescriptors(arguments)
-	options = {
-		loading: true,
-		qs: true,
-		showError: true,
-		...options
-	}
 
-	const {
-		headers,
-		...configRes
-	} = config
-
-	const token = store.state.user.token
-
-	options.loading && startLoading()
-	let closeLoading = () => {
-		options.loading && endLoading()
-		closeLoading = null
-	}
-
-	return promisifyRequest('request', {
-		cancelToken: source.token,
-		method,
-		url: url.startsWith('http') ? url : BASE_URL + url,
-		data: params,
-		header: {
-			'Content-Type': method.toUpperCase() === 'GET' ? CONTENT_TYPES.JSON : options.qs ?
-				CONTENT_TYPES.FORM_URLENCODED : CONTENT_TYPES.JSON,
+export const request = createRequest({
+	baseURL: BASE_URL,
+	headers() {
+		const token = store.state.user.token
+		return {
 			'source': 'miniProgram',
 			'deliveryType': store.state.deliveryType,
 			...(token && {
 				[TOKEN_KEY]: token
 			}),
-			...headers
-		},
-		...configRes
-	}).then(res => {
-		closeLoading()
-		return checkStatus(res, options, () => request(...argumentsCache))
-	}).catch(e => {
-		closeLoading && closeLoading()
-		return Promise.reject(e)
-	})
-}
+		}
+	}
+}, {
+	resInterceptor,
+	errInterceptor
+})
 
 export default request
