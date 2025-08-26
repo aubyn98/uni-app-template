cf9a767b67349e163741f6f74f7213aeaf29f92f
aubyn <aubyn@vip.qq.com>
2025-08-26 17:25:37 +0800

    update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import App from './App'
 import store from '@/store'
 import * as apis from '@/common/apis'
 import * as utils from '@/common/utils'
+import * as CONFIG from '@/common/config'
 import customUI from '@/components/custom-ui'
 import uview from '@/components/uview-ui';
 import {
@@ -11,6 +12,7 @@ import {
 
 Vue.prototype.$utils = utils
 Vue.prototype.$apis = apis
+Vue.prototype.$CONFIG = CONFIG
 Vue.use(customUI).use(uview).mixin(global)
 
 
